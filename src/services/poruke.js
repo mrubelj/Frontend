35333e620892c013c765283e79e0725d276addad
Marta Rubelj <mrubelj@pmfst.hr>
2020-10-21 17:19:45 +0200

    novi port

--- src/services/poruke.js
+++ src/services/poruke.js
@@ -1,6 +1,7 @@
 	
 import axios from 'axios'
-const osnovniUrl = 'http://localhost:3001/api/poruke'
+//const osnovniUrl = 'http://localhost:3001/api/poruke'
+const osnovniUrl = 'https://mrub-poruke.herokuapp.com/'
  
 const dohvatiSve = () => {   
     const promise = axios.get(osnovniUrl);
